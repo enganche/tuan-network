9d19600930d40b5795c2fb6d8866056adc7263ca
Anhtuanzzz <62417928+Anhtuanzzz@users.noreply.github.com>
2021-12-21 08:43:08 +0700

    init project

--- App.js
+++ App.js
@@ -1,10 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
 
 export default function App() {
+  const [coin, setCoin] = useState(0);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.header}>App lùa gà</Text>
+      <Text style={styles.coin}>{coin}TC</Text>
+      <Button
+        onPress={() => setCoin(coin + 1)}
+        title="Điểm danh"
+        color="#841584"
+        style={styles.button}
+        accessibilityLabel="Điểm danh hàng ngày"
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +27,16 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  header: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  coin: {
+    fontSize: 60,
+    fontWeight: 'bold',
+  },
+  button: {
+
   },
 });
